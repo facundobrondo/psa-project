f51cb39ac9813e077ced4f5e1ec043a0ad6dc4aa
Ulises Nieva <101995343+UlisesN98@users.noreply.github.com>
2024-06-29 14:07:43 -0300

    Comentario api recursos

--- src/main/java/proyectos/ProjectsApp.java
+++ src/main/java/proyectos/ProjectsApp.java
@@ -151,6 +151,7 @@ public class ProjectsApp {
         return new RestTemplate();
     }
 
+	// Endpoint auxiliar al que se recurrio provisoriamente debido a problemas de integracion con el original. Actualmente no se utiliza.
 	@GetMapping("/recursos")
     public ResponseEntity<String> callExternalApi() {
         String apiUrl = "https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/recursos-psa/1.0.0/m/api/recursos";
